9d4dc488cfa8c2740e79c731a5d2c530f70f3f94
Mark Christian <markc@twitter.com>
2013-08-06 09:52:52 -0700

    Actually implement proper blink frequency per http://en.wikipedia.org/wiki/Blink_element

--- blink.js
+++ blink.js
@@ -1,17 +1,22 @@
 ;(function() {
 
   var blinkStateVisible = true;
+  var kBlinkVisibilityDuration = 750;
+  var kBlinkHiddenDuration = 250;
+
   function toggleBlinking() {
     blinkStateVisible = !blinkStateVisible;
     var visibilityState = blinkStateVisible ? 'visible' : 'hidden'
+    var timeoutDuration = blinkStateVisible ? kBlinkVisibilityDuration : kBlinkHiddenDuration;
     var blinkElements = Array.prototype.slice.apply(document.querySelectorAll('blink'));
 
     blinkElements.forEach(function(elem) {
       elem.style.visibility = visibilityState;
     });
+
+    setTimeout(toggleBlinking, timeoutDuration);
   };
 
-  var kBlinkInterval = 500;
-  var blinkTimer = setInterval(toggleBlinking, kBlinkInterval);
+  setTimeout(toggleBlinking, kBlinkVisibilityDuration);
 
 })();
